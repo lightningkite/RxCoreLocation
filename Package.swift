26a5f4941d624a308690db1b137462a213f816a6
Shane Thompson <shane@meosphere.com>
2025-04-30 14:19:35 -0600

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "RxCoreLocation", targets: ["RxCoreLocation"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
+        .package(url: "https://github.com/LightningKite/RxSwift.git", from: "6.2.4-LK")
     ],
     targets: [
         .target(name: "RxCoreLocation", dependencies: ["RxSwift", "RxCocoa"], path: "Sources")
